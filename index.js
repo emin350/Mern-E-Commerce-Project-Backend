ce76a3cd76a7c90d33b9a9d42eaef315536d5217
Emin Ar <82047546+emin350@users.noreply.github.com>
2023-01-21 10:53:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const mongoose = require('mongoose')
+const cors =  require('cors')
+const authController = require('./controllers/authController')
+const productController = require('./controllers/productController')
+const uploadController = require('./controllers/uploadController')
+const app = express()
+
+mongoose.connect(process.env.MONGO_URL, () => console.log("DB is successfully connected"))
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+//inside public folder
+app.use('/images',express.static('public/images'))
+
+app.use('/auth',authController)
+app.use('/product',productController)
+app.use('/upload',uploadController)
+
+app.listen(process.env.PORT,() => console.log("server has been started successfuly"))
